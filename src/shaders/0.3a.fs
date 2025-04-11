68d287865deeb7d54e99b56ca804dae0cd5f8326
z-cog <144469531+z-cog@users.noreply.github.com>
2025-04-11 10:53:04 -0600

    Uploaded textures and shaders

---
+++ src/shaders/0.3a.fs
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+	FragColor = vec4(ourColor, 1.0); //* mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+}
